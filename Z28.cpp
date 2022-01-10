c8f91451ffb9d771833bc2ce3ddd4f8fd565931d
ShenekPL <90441718+ShenekPL@users.noreply.github.com>
2022-01-10 16:28:16 +0100

    Add files via upload

---
+++ Z28.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main(int argc, char *argv[])
+{
+ for ( int i=4;i<=34;i+=2)
+{ 
+ cout<<i<<'.';
+}
+ cout<<endl;
+ system("PAUSE");
+ return EXIT_SUCCESS;
+}
+
